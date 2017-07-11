b33e14ffd4fd21e9dde22a28c27d510ae4de6ae3
MarwaDesouky <marwadesouky96@gmail.com>
2017-07-11 12:54:35 +0200

    hello there

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,22 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello  Firebase!");
-});
+exports.sanitizePost=functions.database
+.ref('/posts/{pushId}')
+onWrite(event => {
+    const post=event.data.val()
+    if(post.sanitized){
+        return
+    }
+    console.log("sanitized new post"+event.params.pushId)
+    console.log(post)
+    post.sanatized=true
+    post.title=sanitize(post.title)
+    post.body=sanitize(post.body)
+    return event.data.ref.set(post)
+})
+function sanitize(s){
+    var sanitizedText=s
+    sanitizedText=sanitizedText.replace(/\bstupid\b/ig,"wonderful")
+    return sanitizedText
+}
